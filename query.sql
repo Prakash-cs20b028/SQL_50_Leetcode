667ee4b91cb2e6af581293bfb20f71fcc98cdfba
Prakash-cs20b028 <Programmer52525@gmail.com>
2024-04-23 16:25:47 +0530

    Added

---
+++ query.sql
@@ -0,0 +1,5 @@
+select query_name,round(avg(rating/position),2) as quality,
+round((avg(rating<3)) * 100,2) as poor_query_percentage
+from Queries
+where query_name is not null
+group by (query_name); 
